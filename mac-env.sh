7656798742208e98b222690af55ef7db694a7c90
Scott Smith <scottsmith1@gmail.com>
2025-01-21 22:32:45 -0800

    Add litellm-config.yaml.

--- mac-env.sh
+++ mac-env.sh
@@ -1,8 +1,11 @@
 # Required for the front end to build (default Mac is too new.)
 export PATH="/opt/homebrew/opt/node@22/bin:$PATH"
 
+export OPEN_WEBUI_REPO=$HOME/open-webui
+
 # Required for the dev environment to work.
-export FRONTEND_BUILD_DIR=/Users/ssmith/open-webui/build
+export FRONTEND_BUILD_DIR=$OPEN_WEBUI_REPO/build
+export DATA_DIR=$OPEN_WEBUI_REPO/backend/data
 
 # Redirect to LiteLLM where all models are exposed as OpenAI compatible interfaces.
 export OPENAI_API_KEY=none
